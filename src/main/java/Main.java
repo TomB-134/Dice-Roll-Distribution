b4d58b3245e70804647a056a0601749aac2e5bd5
Thomas Baum <thomasbaum134@outlook.com>
2018-09-15 13:09:51 +1000

    IDE to github repo.

---
+++ src/main/java/Main.java
@@ -0,0 +1,73 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Main extends Application {
+
+    private BarChart distribution;
+
+    private int numOfRolls = 10000;
+    private int diceNum = 24;
+    private int[] data;
+
+    @Override
+    public void start(Stage window) {
+        rollDice();
+        drawGraph();
+
+        VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        root.getChildren().add(distribution);
+
+        Scene scene = new Scene(root);
+
+        window.setTitle("Dice Roll Distribution");
+        window.setScene(scene);
+        window.show();
+    }
+
+    private void rollDice() {
+        data = new int[diceNum * 6];
+
+        for (int i = 0; i < numOfRolls; i++) {
+            int total = 0;
+            for (int j = 0; j < diceNum; j++) {
+                total += ThreadLocalRandom.current().nextInt(1, 7);
+            }
+
+            System.out.println(total);
+            data[total - 1] += 1;
+        }
+
+    }
+
+    @SuppressWarnings("unchecked")
+    private void drawGraph() {
+        CategoryAxis xAxis = new CategoryAxis();
+        xAxis.setLabel("Roll Result");
+
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("Frequency");
+
+        XYChart.Series dataSeries = new XYChart.Series();
+
+        for (int i = 0; i < data.length; i++) {
+            dataSeries.getData().add(new XYChart.Data<>("" + i, data[i]));
+        }
+
+        distribution = new BarChart<>(xAxis, yAxis);
+        distribution.getData().add(dataSeries);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
